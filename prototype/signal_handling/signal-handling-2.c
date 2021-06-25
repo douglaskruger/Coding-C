08f34ec07d7c39a161c015d263f28f3eafba4458
Douglas Kruger <douglas.d.kruger@gmail.com>
2021-06-25 12:58:57 +1200

    Signal Handling
    Initial version

--- prototype/signal_handling/signal-handling-2.c
+++ prototype/signal_handling/signal-handling-2.c
@@ -1,3 +1,11 @@
+/* =============================================================================
+   Copyright © 2019 Skynet Consulting Ltd.
+
+   This file is the property of Skynet Consulting Ltd. and shall not be reproduced,
+   copied, or used as the basis for the manufacture or sale of equipment without
+   the express written permission of Skynet Consulting Ltd.
+   =============================================================================
+*/
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
