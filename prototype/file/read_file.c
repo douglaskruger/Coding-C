18f55c68cdd52a8e5462f97a707b19c322bc82a6
Douglas Kruger <douglas.d.kruger@gmail.com>
2021-06-27 09:18:39 +1200

    Initial files

--- prototype/file/read_file.c
+++ prototype/file/read_file.c
@@ -1,3 +1,13 @@
+/* =============================================================================
+   Copyright © 2020 Skynet Consulting Ltd.
+
+   This file is the property of Skynet Consulting Ltd. and shall not be reproduced,
+   copied, or used as the basis for the manufacture or sale of equipment without
+   the express written permission of Skynet Consulting Ltd.
+   =============================================================================
+*/
+static const char module_id[] __attribute__((used)) = "$Id: read_file.c$";
+
 #include <stdio.h>
 #include <string.h>
 
@@ -6,7 +16,7 @@ int main()
     FILE * database;
     char buffer [1024];
 
-    database = fopen("modbus.csv","r");
+    database = fopen("test.csv","r");
 
     if (NULL == database) {
         perror("opening database file");
