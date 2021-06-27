5dd2799c51c499a7c7ecdb194d341945499267c1
Douglas Kruger <douglas.d.kruger@gmail.com>
2021-06-28 11:48:59 +1200

    Updated

--- prototype/math/rightmostdiffbit.c
+++ prototype/math/rightmostdiffbit.c
@@ -1,3 +1,13 @@
+/* =============================================================================
+   Copyright © 2020 Skynet Consulting Ltd.
+
+   This file is the property of Skynet Consulting Ltd. and shall not be reproduced,
+   copied, or used as the basis for the manufacture or sale of equipment without
+   the express written permission of Skynet Consulting Ltd.
+   =============================================================================
+*/
+static const char module_id[] __attribute__((used)) = "$Id: rightmostdiffbit.c$";
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
