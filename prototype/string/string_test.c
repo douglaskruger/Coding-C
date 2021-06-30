a37a8df6126142d6cfc2b96c9aea6aa14c4f8699
Douglas Kruger <douglas.d.kruger@gmail.com>
2021-07-01 11:06:34 +1200

    Update string_test.c

--- prototype/string/string_test.c
+++ prototype/string/string_test.c
@@ -1,6 +1,5 @@
 /* =============================================================================
    Copyright © 2020 Skynet Consulting Ltd.
-
    This file is the property of Skynet Consulting Ltd. and shall not be reproduced,
    copied, or used as the basis for the manufacture or sale of equipment without
    the express written permission of Skynet Consulting Ltd.
